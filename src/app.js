fbbfe1a758fbf78a306a085f43cc70bc2421fa71
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-03-12 18:01:01 +0200

    still working with lab

---
+++ src/app.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Categories from './components/categories.js';
+import Products from './components/products.js';
+
+export default props => {
+  return (
+    <>
+      <Categories />
+      <Products />
+    </>
+  );
+};
